bf04050fa990f31bf5a63141551624223c99fed1
LSequeira <lucassequeiradaniel@gmail.com>
2020-01-04 00:38:18 -0300

    Update Menu
    Se cambio el href brand por Link component

--- frontend/src/partials/Menu.js
+++ frontend/src/partials/Menu.js
@@ -8,11 +8,9 @@ function Menu() {
           {/* Main Sidebar Container */}
           <aside className="main-sidebar sidebar-dark-primary elevation-4">
             {/* Brand Logo */}
-            <a href="index3.html" className="brand-link">
-              <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3"
-                style={{opacity: '.8'}} />
+            <Link className="brand-link" to="/">
               <span className="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+            </Link>
             {/* Sidebar */}
             <div className="sidebar">
               {/* Sidebar Menu */}
